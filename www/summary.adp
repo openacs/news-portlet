fa5b1f047086961544ba2cbb858556574a220c42
Jeff Davis <davis@xarg.net>
2004-06-29 10:18:40 +0000

    merge of changes on oacs-5-1 branch between tags jcd-merge-5-1-20040517 and jcd-merge-5-1-20040628 to HEAD

--- www/summary.adp
+++ www/summary.adp
@@ -1,5 +1,5 @@
 <% # This is the default template to render news items in admin/. Comments are not shown here %>
-<h3>@publish_title@</h3>
+<h3>@publish_title@ - <small>@publish_date@</small></h3>
 <blockquote>
 @publish_body;noquote@
 @more_link;noquote@
--- www/summary.tcl
+++ www/summary.tcl
@@ -72,8 +72,9 @@ if { $item_exist_p } {
 #This is a summary in the portlet we don't want it to be too long.
 
 set more_link ""
-if { [string length $publish_body] > 1000 } {
-    set publish_body [string_truncate -len 1000 -- $publish_body]
+set summary_length [news_portlet::get_summary_length]
+if { [string length $publish_body] > $summary_length } {
+    set publish_body [string_truncate -len $summary_length -- $publish_body]
     set more_link "<br><b>&raquo;</b> <a href=\"$url\">[_ news-portlet.Read_more]</a>"
 }
 

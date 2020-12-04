55c0ed420574b32877926a084bf3b9f8e36c93b0
Hector Romojaro <hector.romojaro@gmail.com>
2020-12-04 10:27:56 +0000

    Whitespace changes

--- www/news-portlet.tcl
+++ www/news-portlet.tcl
@@ -82,17 +82,26 @@ if { $inside_comm_p } {
     if { $display_subgroup_items_p } {
         set subgroup_package_ids [db_list select_subgroup_package_ids {
             select package_id
-            from apm_packages p right outer join
-               ( WITH RECURSIVE site_node_tree AS (
-                    select node_id, parent_id, object_id from site_nodes where node_id = :root_id
-                 UNION ALL
-                    select c.node_id, c.parent_id, c.object_id from site_node_tree tree, site_nodes as c
-                    where  c.parent_id = tree.node_id
-                 )
-		 select * from site_node_tree n) site_map
-            on site_map.object_id = p.package_id
-            where package_key = 'news'
-	    and (site_map.object_id is null or acs_permission.permission_p(site_map.object_id, :user_id, 'read') = 't')
+              from apm_packages p right outer join
+                    ( WITH RECURSIVE site_node_tree AS (
+                        select node_id,
+                               parent_id,
+                               object_id
+                          from site_nodes
+                         where node_id = :root_id
+                     UNION ALL
+                         select c.node_id,
+                                c.parent_id,
+                                c.object_id
+                           from site_node_tree tree,
+                                site_nodes as c
+                          where c.parent_id = tree.node_id
+                    )
+                    select * from site_node_tree n) site_map
+                        on site_map.object_id = p.package_id
+                     where package_key = 'news'
+                       and (site_map.object_id is null
+                            or acs_permission.permission_p(site_map.object_id, :user_id, 'read') = 't')
         }]
         if {[llength $subgroup_package_ids] > 0} {
             set one_instance_p 0

61e5a02352e4bd1f4df9758dfccde64855388d20
Antonio Pisano <antonio@elettrotecnica.it>
2023-03-30 16:42:06 +0000

    Basic test of portlet rendering

--- tcl/test/news-portlet-procs.tcl
+++ tcl/test/news-portlet-procs.tcl
@@ -7,6 +7,45 @@ ad_library {
 }
 
 aa_register_case -procs {
+    news_admin_portlet::show
+    news_portlet::show
+} -cats {
+    api
+    smoke
+} render_portlet {
+    Test the rendering of the portlets
+} {
+    aa_run_with_teardown -rollback -test_code {
+        set package_id [site_node::instantiate_and_mount \
+                            -package_key news \
+                            -node_name __test_news_portlet]
+
+        set cf [list \
+                    package_id $package_id \
+                    shaded_p false \
+                   ]
+
+        foreach portlet {news_admin_portlet news_portlet} {
+            aa_section $portlet
+
+            set portlet [acs_sc::invoke \
+                             -contract portal_datasource \
+                             -operation Show \
+                             -impl $portlet \
+                             -call_args [list $cf]]
+
+            aa_log "Portlet returns: [ns_quotehtml $portlet]"
+
+            aa_false "No error was returned" {
+                [string first "Error in include template" $portlet] >= 0
+            }
+
+            aa_true "Portlet looks like HTML" [ad_looks_like_html_p $portlet]
+        }
+    }
+}
+
+aa_register_case -procs {
         news_portlet::link
         news_portlet::get_pretty_name
         news_admin_portlet::link
